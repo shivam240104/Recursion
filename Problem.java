29f6163fed50a688d89d8d79486666556c3a6e65
shivam240104 <mauryatanmay845@gmail.com>
2024-09-07 09:50:27 +0530

    print sum of n naturel number

--- Problem.java
+++ Problem.java
@@ -22,22 +22,34 @@ public static void printInc(int n){
     System.out.print(n+ " ");
 }
 
-//  <<<<<problem-2>>>>>>   <<<<<< print incrising number from 1 to n>>>>>>
+//  <<<<<problem-3>>>>>>   <<<<<< print incrising number from 1 to n>>>>>>
 
     public static int fact(int n){
         if(n==0){
             return 1;
         }
-        int fnm1=fact(n-1);
+        // int fnm1=fact(n-1);
         int fn = n*fact(n-1);
          return fn;
     }
+
+//  <<<<<problem-4>>>>>>   <<<<<< print dum of n numbers>>>>>>
+
+public static int sum(int n){
+    if(n==1){
+        return 1;
+    }
+     int Sum=n+sum(n-1);
+     return Sum;
+}
+
 
     public static void main (String []args){
         int n=10;
         printDec(n);
         printInc(n);
 
-        System.out.println(fact(n));
+        System.out.println(fact(5));
+        System.out.println(sum(n));
     }
 }
