eda66141098a8b2af86c1f031326cbdcb041e4f0
shivam240104 <mauryatanmay845@gmail.com>
2024-09-07 09:20:59 +0530

    print increasing number from 1 to n

--- Problem.java
+++ Problem.java
@@ -3,16 +3,28 @@ public class Problem{
 //  <<<<<problem-1>>>>>>   <<<<<< print decrising number from n to 1>>>>>>
 public static void printDec(int n){
     if(n==1){
-        System.out.println("1");
+        System.out.print("1");
         return ;
 
     }
-    System.out.println(n);
+    System.out.print(n+" ");
     printDec(n-1);
+}
+
+//  <<<<<problem-2>>>>>>   <<<<<< print incrising number from 1 to n>>>>>>
+
+public static void printInc(int n){
+    if(n==1){
+        System.out.print("1");
+        return;
+    }
+    printInc(n-1);
+    System.out.print(n+ " ");
 }
 
     public static void main (String []args){
         int n=10;
         printDec(n);
+        printInc(n);
     }
 }
