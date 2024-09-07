99b6fb2dbbde30fc11ebdbf8d5e5a86dfd77303e
shivam240104 <mauryatanmay845@gmail.com>
2024-09-07 09:00:40 +0530

    basic recursion problem 1

---
+++ Problem.java
@@ -0,0 +1,18 @@
+public class Problem{
+
+//  <<<<<problem-1>>>>>>   <<<<<< print decrising number from n to 1>>>>>>
+public static void printDec(int n){
+    if(n==1){
+        System.out.println("1");
+        return ;
+
+    }
+    System.out.println(n);
+    printDec(n-1);
+}
+
+    public static void main (String []args){
+        int n=10;
+        printDec(n);
+    }
+}
